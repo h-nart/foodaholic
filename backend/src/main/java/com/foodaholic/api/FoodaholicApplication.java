3b6289c69acbdac45454115cac76c7e9284627b4
nhaddad <hnart96@gmail.com>
2025-11-06 16:46:29 +0300

    Bootstrap backend

---
+++ backend/src/main/java/com/foodaholic/api/FoodaholicApplication.java
@@ -0,0 +1,13 @@
+package com.foodaholic.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodaholicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodaholicApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/foodaholic/api/FoodaholicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodaholic.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodaholicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
